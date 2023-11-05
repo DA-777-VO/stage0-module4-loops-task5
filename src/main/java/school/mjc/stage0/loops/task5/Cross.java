3a1fa8d02a93ff13395a2048df6a227ef979fc7b
David Apakin <apakin.david@gmail.com>
2023-11-05 22:10:58 +0400

    4.2.5

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,5 +3,23 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
 
+
+        if (sideLength % 2 == 0) {
+            System.out.println("Size must be odd.");
+            return;
+        }
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i == j || i + j == sideLength - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,33 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+
+        if (height % 2 == 0) {
+            System.out.println("Size must be odd.");
+            return;
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == j || i + j == height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = height - 2; i >= 0; i--) {
+            for (int j = 0; j < height; j++) {
+                if (i == j || i + j == height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
